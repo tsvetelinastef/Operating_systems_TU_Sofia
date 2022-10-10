ade9700f99487e5d345a4596384b017b0496db3b
Tsvety Stefanova <73800200+tsvetelinastef@users.noreply.github.com>
2022-10-10 20:08:27 +0300

    Update 2017CorrectionTest_Folder1stTest.cpp

--- 2017CorrectionTest_Folder1stTest/2017CorrectionTest_Folder1stTest.cpp
+++ 2017CorrectionTest_Folder1stTest/2017CorrectionTest_Folder1stTest.cpp
@@ -1,8 +1,29 @@
-﻿/*
-Да се напише многонишкова програма,
+/*
+To write a multi-threaded/ многонишкова
+program,which starts 4 threads.
 която стартира 4 нишки. 
-Всяка от нишките има за цел да проверява
-стойност от общата им памет и трябва да
+Each of the threads aims to check
+value from their shared memory and should
+make a decision depending on this value.
+Each thread has a parameter k.
+When the value in shared memory does not
+divides an integer by k, then the string
+increments the shared cell by one
+and falls asleep in k seconds. If the value
+of the common variable is a multiple of k, it
+the same is incremented
+with k+1, and the thread sleeps for 1 second,
+but if the value of the shared variable
+is also divisible by the square of k, the thread
+adds 2k+1 and doesn't sleep.
+
+
+
+Да се напише многонишкова програма,
+която стартира 4 нишки.
+Всяка от нишките има за цел да 
+проверява стойност от от общата им
+памет и трябва да 
 вземе решение в зависимост от тази стойност.
 Всяка нишка притежава параметър k.
 Когато стойносста в общатата памет не се 
